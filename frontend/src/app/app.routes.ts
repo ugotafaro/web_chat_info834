2aa7497967147040fb06b9c215f8dc690b634604
ugotafaro <ugo.tafaro@etu.univ-savoie.fr>
2024-02-16 15:01:33 +0100

    [FEATURES] : display chat

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './login/login.component';
 import { CategoriesToolbarComponent } from './categories-toolbar/categories-toolbar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
-    { path: '', component: CategoriesToolbarComponent },
+    { path: '', component: ChatComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignUpComponent },
     { path: '**', component: PageNotFoundComponent },
