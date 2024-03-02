1b80310a8d372716251f18d0ba4e1d270b2be295
hug0be <83722580+hug0be@users.noreply.github.com>
2024-03-02 16:37:29 +0100

    [FIX] Remove debug db manipulation

--- backend/db.js
+++ backend/db.js
@@ -7,11 +7,4 @@ const db = mongoose.connection;
 db.on('error', () => { console.error('[INFO] Echec de connexion à MongoDB') });
 db.once('open', () => { console.log('[INFO] Connexion à MongoDB réussie') });
 
-// TODO : Remove
-async function createAndSaveMessage() {
-    await Message.deleteMany({});
-    let message = await Message.create({content: 'Hi !', sender: '65cf88d47454c0b8fec5de40', conversation_name: 'test', receivers: ['65ddc1ac007f09cc725ad3a6', '65de3ae5aa5600d1776d9c4b']});
-}
-
-createAndSaveMessage();
 module.exports = db;
