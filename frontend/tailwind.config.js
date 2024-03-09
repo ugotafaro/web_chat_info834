249c773d581c638de08cfc160173f2d8abaf9ee0
hug0be <83722580+hug0be@users.noreply.github.com>
2024-03-09 14:06:29 +0100

    [FEATURE] "new conversation" modal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          50: '#F6F5FF',
+          100: '#EDEBFE',
+          200: '#DCD7FE',
+          300: '#CABFFD',
+          400: '#AC94FA',
+          500: '#9061F9',
+          600: '#7E3AF2',
+          700: '#6C2BD9',
+          800: '#5521B5',
+          900: '#4A1D96',
+        }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
