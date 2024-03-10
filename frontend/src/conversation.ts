cabc034256cf9bf0a18b78a0eb8fbb6cfbe4eb93
ugotafaro <ugo.tafaro@etu.univ-savoie.fr>
2024-03-10 17:35:36 +0100

    [FEATURES] : view conversation

--- frontend/src/conversation.ts
+++ frontend/src/conversation.ts
@@ -3,15 +3,15 @@ import { User } from "./user";
 
 export class Conversation {
     id: number ;
+    name : string;
     content: string;
-    timestamp: string;
     users: User[];
     messages: Message[];
 
-    constructor(id: number, content: string, timestamp: string, users: User[], messages: Message[]) {
+    constructor(id: number, name: string, content: string,  users: User[], messages: Message[]) {
         this.id = id;
+        this.name = name;
         this.content = content;
-        this.timestamp = timestamp;
         this.users = users;
         this.messages = messages;
     }
