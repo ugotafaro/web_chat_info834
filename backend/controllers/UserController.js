14a062615bb60ad5eadd5120aa3e337355e0814e
ugotafaro <ugo.tafaro@etu.univ-savoie.fr>
2024-03-12 09:57:13 +0100

    [FEATURES] : recup id user sender

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,9 +2,8 @@ const User = require('../models/UserModel');
 const { handleErrors } = require('../util');
 
 const get = (req, res) => {
-
     // Check if user is the same as the one in the token
-    if (req.user.id !== req.params.id) return handleErrors(res, 403, 'Forbidden');
+    //if (req.user.id !== req.params.id) return handleErrors(res, 403, 'Forbidden');
 
     res.json({ user:req.user });
 };
